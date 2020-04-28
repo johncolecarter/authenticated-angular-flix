715c861233e022671a30a95cfb1f789310905d8e
johncolecarter <colecarter7777@gmail.com>
2020-04-28 11:28:52 -0500

    organized main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,13 +7,17 @@ import { MovieService } from '../services/movie.service';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+
+  searchResults = [];
+  myMovieList = [];
+  title = 'Angular Flix';
+
   constructor(private movieService: MovieService) { }
+
+
   ngOnInit(): void {
     this.myMovieList = this.movieService.getMovieList();
     this.searchResults = this.movieService.getSearchResults();
     this.movieService.loadMovieList();
   }
-  searchResults = [];
-  myMovieList = [];
-  title = 'Angular Flix';
 }
